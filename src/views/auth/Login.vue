14ca68c6d76a6c54b5d9006055f4cdf2fecd582a
Marek Galganski <marek.galganski@boncard.pl>
2020-11-30 00:29:37 +0100

    added vuetify

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-row
+      justify="center"
+    >
+      <v-col
+        md="6"
+      >
+      <v-banner
+        elevation="7"
+        single-line
+        sticky
+        align="center"
+      >
+        LOG IN
+      </v-banner>
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+        >
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="Name"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+
+          <v-select
+            v-model="select"
+            :items="items"
+            :rules="[v => !!v || 'Item is required']"
+            label="Item"
+            required
+          ></v-select>
+
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to continue!']"
+            label="Do you agree?"
+            required
+          ></v-checkbox>
+
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate"
+          >
+            Validate
+          </v-btn>
+
+          <v-btn
+            color="error"
+            class="mr-4"
+            @click="reset"
+          >
+            Reset Form
+          </v-btn>
+
+          <v-btn
+            color="warning"
+            @click="resetValidation"
+          >
+            Reset Validation
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+      <h1>Register</h1>
+      <router-link :to="{name: 'login'}">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
