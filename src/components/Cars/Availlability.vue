60c527e9bdf160dc4fe7b123d4daaa5e04eddf70
Marek Galganski <marek.galganski@boncard.pl>
2020-12-15 23:24:24 +0100

    added form to checkAvailability

--- src/components/Cars/Availlability.vue
+++ src/components/Cars/Availlability.vue
@@ -1,12 +1,17 @@
 <template>
   <v-container>
       <v-row>
+        <h2>Check Availability</h2>
+         <v-form
+          ref="availabityForm"
+        >
         <v-col
           cols="12"
           md="6"
         >
           <v-text-field
-            
+          v-model="from"
+          label="from"
             required
           ></v-text-field>
         </v-col>
@@ -16,11 +21,19 @@
           md="6"
         >
           <v-text-field
-            
+            v-model="to"
+            label="to"
             required
+            @keyup.enter="checkAvailability()"
           ></v-text-field>
         </v-col>
-
+        <v-btn
+            color="primary"
+            @click="checkAvailability()"
+          >
+            Check
+          </v-btn>
+        </v-form>
       </v-row>
   </v-container>
 </template>
@@ -29,7 +42,13 @@
 export default {
   data() {
     return {
-      
+      from: null,
+      to: null
+    }
+  },
+  methods: {
+    checkAvailability() {
+      alert('zd');
     }
   }
 }
